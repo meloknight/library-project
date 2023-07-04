5e40158dcd8cbce242aa329ef9c3b2e366b8dc6e
Cornelius <49502879+meloknight@users.noreply.github.com>
2023-07-04 07:44:53 -0700

    begin initial setup of app.js and index.html

---
+++ app.js
@@ -0,0 +1,22 @@
+let myLibrary = [
+    // {title: "The Hobbit", author: "J.R.R. Tolkien", pages: 123, read: "book not read", rating: 7}, 
+    // {title: "The Wheel of Time", author: "Robert Jordan", pages: 10000, read: "book read", rating: 9}, 
+    // {title: "Billy's Travails", author: "Billy Boblin", pages: 2, read: "book read", rating: 2}, 
+];
+
+function Book() {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.rating = rating;
+};
+
+
+
+function addBookToLibrary() {
+
+}
+
+
+console.log(myLibrary);
